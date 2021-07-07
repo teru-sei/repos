8ca8b85b7bee14d1e5b626230309af9c8a67d359
teru-sei <hfc.3591@gmail.com>
2021-07-07 15:16:39 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+  end
 
-  belongs_to :user
+  has_many :cafes
   #has_many :comments
   #has_many :likes
 end
