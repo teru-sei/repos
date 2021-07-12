7b85d666305c412074280245138c98f5de0a5788
teru-sei <hfc.3591@gmail.com>
2021-07-12 13:56:43 +0900

    記述変更

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -37,25 +37,25 @@ class CafesController < ApplicationController
       @region = "北海道"
     elsif params[:region] == "3"
       @cafe = Cafe.where(prefecture_id: [3..8])
-      @region = "東北"
+      @region = "東北地方"
     elsif params[:region] == "4"
       @cafe = Cafe.where(prefecture_id: [9..15])
-      @region = "関東"
+      @region = "関東地方"
     elsif params[:region] == "5"
       @cafe = Cafe.where(prefecture_id: [16..24])
-      @region = "中部"
+      @region = "中部地方"
     elsif params[:region] == "6"
       @cafe = Cafe.where(prefecture_id: [25..31])
-      @region = "関西"
+      @region = "関西地方"
     elsif params[:region] == "7"
       @cafe = Cafe.where(prefecture_id: [32..36])
-      @region = "中国"
+      @region = "中国地方"
     elsif params[:region] == "8"
       @cafe = Cafe.where(prefecture_id: [37..40])
-      @region = "四国"
+      @region = "四国地方"
     elsif params[:region] == "9"
       @cafe = Cafe.where(prefecture_id: [41..48])
-      @region = "九州"
+      @region = "九州地方"
     elsif params[:genre] == "1"
       @cafe = Cafe.where(purpose_id: [2])
       @purpose = "コーヒー"
