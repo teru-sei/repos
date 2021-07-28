234f624154c2d71b13c8b7a638d1b9bd737128a9
teru-sei <hfc.3591@gmail.com>
2021-07-28 14:53:35 +0900

    投稿画像表示　仮

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -18,7 +18,8 @@ class Cafe < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   # has_many :likes
-  has_many_attached :image
+  has_many_attached :images
+  has_one_attached :image
 
   def self.search(_search)
     Cafe.all
